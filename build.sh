a977af3ae1e18a63a7ff348566e7a003260201a6
Arif Endro Nugroho <arif_endro@yahoo.com>
2023-03-14 13:42:05 +0700

    Initial checkin.

---
+++ build.sh
@@ -0,0 +1,15 @@
+apt install meson
+apt install cmake pkg-config
+apt install libglib2.0-dev
+apt install libgcrypt20-dev
+apt install libotr5-dev
+apt install libutf8proc-dev
+apt install libperl-dev
+meson build --prefix=/home/arifendronugroho/irssi-git
+ninja -C build
+ninja -C build install
+cd subprojects/openssl-1.1.1l/
+./config
+make
+cd ../../
+ninja -C build install
